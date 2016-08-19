0ce94272bc3402cbd88b296d68f28a14defa99ce
Shaila Jadhav <Shaila Jadhav>
2016-08-18 23:25:18 -0600

    First Commit

---
+++ src/com/java/example/SimpleServlet.java
@@ -0,0 +1,44 @@
+package com.java.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "First Servlet", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Servlet in doGet()");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1> Hi This is servlet <h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/java/example/XMLServlet.java
@@ -0,0 +1,46 @@
+package com.java.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class XMLServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 System.out.println("In XML Servlet");
+		 String userName = request.getParameter("userName");
+		// System.out.println("Hello "+userName);
+		 PrintWriter out = response.getWriter();
+		 out.println("Hi !!"+userName);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 System.out.println("In Post method Servlet");
+		 String userName = request.getParameter("userName");
+		 String fullName = request.getParameter("fullName");
+		 String prof = request.getParameter("prof");
+		 String[] location = request.getParameterValues("loc");
+		 int size =location.length;
+		 PrintWriter out = response.getWriter();
+		 out.println("Hi I known your name "+userName+"Full Name"+fullName+"Your Profession"+prof+"Your at "+location.length+"location");
+		 for(int i=0;i<location.length;i++){
+			 out.println(location[i]);
+			 
+		 }
+		 ServletContext context = request.getServletContext();
+		 HttpSession session = request.getSession();
+		 if(userName!= "" && userName!= null){
+			 session.setAttribute("saved user name", userName);
+			 context.setAttribute("Saved user name", userName);
+		 }
+		 System.out.println("Saved user name in session"+(String)session.getAttribute("saved user name"));
+		 System.out.println("Saved user name in Context"+(String)context.getAttribute("saved user name"));
+			
+	}
+}
